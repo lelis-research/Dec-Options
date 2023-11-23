8af1e1a97446b1d334429d9234fd9e7e4e995891
alikhasi <alikhasi@ualberta.ca>
2023-11-22 19:28:14 -0700

    adding vanilla to scripts

--- scripts/fourrooms.sh
+++ scripts/fourrooms.sh
@@ -1,10 +1,14 @@
 FOURROOMS_EASY_DIR=fourrooms_easy/logs/
 FOURROOMS_MEDIUM_DIR=fourrooms_medium/logs/
 FOURROOMS_HARD_DIR=fourrooms_hard/logs/
+BASELINE_VANILLA=Vanilla
 mkdir -p $FOURROOMS_EASY_DIR
 mkdir -p $FOURROOMS_MEDIUM_DIR
 mkdir -p $FOURROOMS_HARD_DIR
 
 CODE_LOCATION=/home/alikhasi/Dec-Options
 
-sbatch ./fourrooms_easy_training.sh $CODE_LOCATION $FOURROOMS_EASY_DIR
+# training tasks
+sbatch ./fourrooms_training.sh $CODE_LOCATION $FOURROOMS_EASY_DIR
+# Parameter search for vanilla
+sbatch ./fourrooms_parameter_search.sh $CODE_LOCATION $FOURROOMS_EASY_DIR $BASELINE_VANILLA
---
+++ scripts/fourrooms_parameter_search.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=25   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
+#SBATCH --mem=10G        # memory per node
+#SBATCH --time=0-6:00      # time (DD-HH:MM)
+#SBATCH --output=%N-%j.out  # %N for node name, %j for jobID
+#SBATCH --account=def-lelis
+#SBATCH --array=0-9
+
+source ~/minigrid/bin/activate
+
+python3 $1/trainer.py --seed $SLURM_ARRAY_TASK_ID --algorithm PPO --baseline $3 --phase TestTasks --parameter_sweep True --config $1/configs/fourrooms_easy.json --log_path $2
